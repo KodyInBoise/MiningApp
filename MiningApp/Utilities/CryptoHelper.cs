2ae0179d7dad03b9fe2d31992beb2e720b7823a6
Kody Kriner <kody.kriner@gmail.com>
2018-04-28 13:42:33 -0600

    Implementing CryptoHelper Class

---
+++ MiningApp/Utilities/CryptoHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiningApp
+{
+    public class CryptoHelper
+    {
+        public List<string> SupportedList = new List<string>()
+        {
+            "BTC", "ETH", "RDD"
+        };
+
+        public CryptoHelper()
+        {
+
+        }
+    }
+}
